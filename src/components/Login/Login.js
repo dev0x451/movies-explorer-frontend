9804b82fd411998a3304f8fefe92153e6efa87ef
Arkady Schapov <sigma696@gmail.com>
2023-01-19 20:26:01 +0300

    make components responsive

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,3 +1,45 @@
-export default function Login() {
-  return <div>Login</div>
+import { Link } from "react-router-dom"
+import "./Login.css"
+import logo from "../../images/logo.svg"
+
+function Login() {
+  return (
+    <div className="login">
+      <div className="login__container">
+        <Link to="/">
+          <img className="login__logo" src={logo} alt="" />
+        </Link>
+        <h2 className="login__heading">Рады видеть!</h2>
+        <form className="login__form">
+          <label className="login__form-label" htmlFor="email-input">
+            E-mail
+          </label>
+          <input className="login__form-input" id="email-input"></input>
+          <label className="login__form-label" htmlFor="password-input">
+            Пароль
+          </label>
+          <input
+            className="login__form-input login__form-input-password_error"
+            type="password"
+            id="password-input"
+          ></input>
+          <div className="login__form-input-password_validation-message">
+            Что-то пошло не так...
+          </div>
+          <button className="login__form-submit-button">Войти</button>
+        </form>
+
+        <div className="login__bottom-text-container">
+          <span className="login__bottom-text-question">
+            Ещё не зарегистрированы?
+          </span>
+          <Link to="/signup" className="login__bottom-text-link">
+            Регистрация
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+export default Login
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,3 +1,51 @@
-export default function Register() {
-  return <div>Register</div>
+import { Link } from "react-router-dom"
+import "./Register.css"
+import logo from "../../images/logo.svg"
+
+function Register() {
+  return (
+    <div className="register">
+      <div className="register__container">
+        <Link to="/">
+          <img className="register__logo" src={logo} alt="" />
+        </Link>
+        <h2 className="register__heading">Добро пожаловать!</h2>
+        <form className="register__form">
+          <label className="register__form-label" htmlFor="name-input">
+            Имя
+          </label>
+          <input className="register__form-input" id="name-input"></input>
+          <label className="register__form-label" htmlFor="email-input">
+            E-mail
+          </label>
+          <input className="register__form-input" id="email-input"></input>
+          <label className="register__form-label" htmlFor="password-input">
+            Пароль
+          </label>
+          <input
+            className="register__form-input register__form-input-password_error"
+            type="password"
+            id="password-input"
+          ></input>
+          <div className="register__form-input-password_validation-message">
+            Что-то пошло не так...
+          </div>
+          <button className="register__form-submit-button">
+            Зарегистрироваться
+          </button>
+        </form>
+
+        <div className="register__bottom-text-container">
+          <span className="register__bottom-text-question">
+            Уже зарегистрированы?
+          </span>
+          <Link className="register__bottom-text-link" to="/signin">
+            Войти
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+export default Register
